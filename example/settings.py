94ed4862485b760d2922cb1f6b9932f1d0a33063
ArihitoMatsuda <arihito.m@gmail.com>
2019-09-24 20:47:18 +0900

    login into admin

--- example/settings.py
+++ example/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'example',
     'sample.apps.SampleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -51,6 +52,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'example.urls'
+
+LOGIN_REDIRECT_URL = '/admin'
 
 TEMPLATES = [
     {
--- example/urls.py
+++ example/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
+		path('login/', LoginView.as_view(), name='login'),
+		path('sample/', include('sample.urls')),
     path('admin/', admin.site.urls),
 ]
